f7944249ed402c00da73440182dab2609114270f
Facu <facuramirez937@gmail.com>
2023-08-19 01:11:26 -0300

    Segunda PreEntrega JS Marketplace

--- main.js
+++ main.js
@@ -1,147 +1,107 @@
-/*
-//variable numeros
-//let numero = parseFloat(prompt("ingrese un numero:"))
-//alert(numero + 30)
-// variable nombre
-//let nombre = prompt("Ingrese su nombre")
-//alert(nombre + "," + "Como estas")*/
-
-/*
-function nombreFuntion () {
-    codigo a ejecutar por la funcion
+//Clase constructora de los productos
+class Producto {
+    constructor(nombre, precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.vendido = false;
 }
 
-para ejecutar o invocar la funcion hacemos nombreFunction()
-*/
-
-//function saludar() {
-//    console.log("hola");
-//}
-
-/*
-function nombreFuntion (param1, ..., para,3) {
-    codigo a ejecutar por la funcion
-}
-
-para ejecutar o invocar la funcion hacemos nombreFunction(valor1, valor 2, ...)
-*/
-
-/*function saludar(saludo, nombre) {
-    console.log(`${saludo} ${nombre}`);
-}
-
-saludar("hola", "Andres");
-saludar("Bienvenida", "Camila");
-
-
-
-function calculadora(num1, num2, operacion) {
-    switch (operacion) {
-    case "+":
-        return num1 + num2;
-        break;
-
-    case "-":
-        return num1 - num2;
-        break;
-
-    case "*":
-        return num1 * num2;
-        break;
-
-    case "/":
-        return num1 / num2;
-        break;
-
-    default:
-        return "Operacion no identificada";
-        break;
+vender() {
+    this.vendido = true;
     }
 }
 
-//let num1 = parseInt(prompt("Ingrese el numero uno"));
-//let num2 = parseInt(prompt("Ingrese el numero dos"));
-//let operacion = prompt("Ingrese la opreacion");
+//Arreglo donde se guardan los productos creados
+const productos = [];
 
-//let resultado = calculadora(num1, num2, operacion);
-//alert(`El resultado es ${resultado}`);
-*/
+//Productos creados que se agregan al listado anterior
+productos.push(new Producto("Guitarra Criolla", 100000));
+productos.push(new Producto("Guitarra Electrica", 750000));
+productos.push(new Producto("Funda guitarra criolla", 7200));
+productos.push(new Producto("Funda guitarra electrica", 10000));
 
-/*class Producto {
-    constructor(nombre, precio, imagen, cantidad) {
-    this.nombre = nombre;
-    this.precio = precio;
-    this.imagen = imagen;
-    this.cantidad = cantidad;
-    
-}
-    vender() {
-        if(this.cantidad ===0) {
-            console.log("Sin stock")
-        } else {
-            this.cantidad -= 1; // this.cantidad = this.cantidad - 1
-        }
+
+//Mensaje emergente para que el usuario interactue con la busqueda de productos de la página
+let nombre = prompt("Ingrese el nombre del producto a consultar. Ej: Guitarra Criolla, Guitarra Electrica, etc.");
+
+while (nombre != "ESC") {
+
+let producto;
+
+
+for (const item of productos) {
+    if (item.nombre === nombre) {
+    producto = item;
     }
-
 }
-
-const producto1 = new Producto(
-    "Guitarra Eléctrica Les Paul Egr 200",
-    867212,
-    "http://asjkfdgh",
-    5
-);
-
-console.log(producto1);
-producto1.vender();
-producto1.vender();
-producto1.vender();
-producto1.vender();
-producto1.vender();
-producto1.vender();
-console.log(producto1);
-*/
-
-//--------------------------   APUNTES DE CLASE, HASTA CLASE NUMERO 4 INCLUIDA------------------------------
-
-//funcion flecha con varibles y constantes - calculos de futuros precios de guitarras
-const suma = (a, b) => a + b;
-const resta = (a, b) => a - b;
-const iva = (x) => x * 0.21;
-
-let precioProducto = 11000;
-let descuento = 1100;
-
-let nuevoPrecio = resta(suma(precioProducto, iva(precioProducto)), descuento);
-console.log(nuevoPrecio);
-
-
-//Ciclo con condicional
-let entrada = prompt("Ingresar un nombre");
-//Se ingresa "ESC" para finalizar el bucle
-while (entrada != "ESC") {
-    switch (entrada) {
-        case "Facu":
-        alert("HOLA FACU");
-        break;
-        case "Pedro":
-        alert("HOLA PEDRO");
-        break;
-        default:
-        alert("USUARIO NO IDENTIFICADO - INGRESE (ESC) PARA FINALIZAR");
-        break;
-}
-entrada = prompt("Ingresar un nombre");
-}
-
-//condicional
-let edad = parseInt(prompt("Ingrese su edad"));
-
-if (edad >= 18) {
-    alert("Edad confirmada - Puedes realizar compras en la página");
+//Condicionales de interacción de usuario con busqueda de la página
+if (producto) {
+    let mensaje = `
+        Nombre: ${producto.nombre}
+        Precio: $${producto.precio}
+    `;
+//Mensaje de resultado de busqueda y bucle
+    alert(mensaje);
 } else {
-    alert("Edad confirmada - No puedes realizar compras en la página");
+    alert("El producto no se encuentra disponible. Si desea salir del buscador escriba ESC");
 }
 
+nombre = prompt("Ingrese el nombre del producto a consultar. Ej: Guitarra Criolla, Guitarra Electrica, etc.");
+}
 
+//Listado de productos disponibles para consultar y/o comprar
+const elementos = [
+    { id: 1, nombre: "Guitarra Criolla", precio: 100000 },
+    { id: 2, nombre: "Guitarra electrica", precio: 750000 },
+    { id: 3, nombre: "Funda guitarra criolla", precio: 7200 },
+    { id: 4, nombre: "Funda guitarra electrica", precio: 10000 },
+];
+//Detalle listado de nombres de prouctos
+const nombres = elementos.map((item) => item.nombre);
+console.log(nombres);
+//Detalle listado de precios de productos y variaciones del iva
+const precios = elementos.map((item) => item.precio);
+console.log(precios);
 
+const nuevosPrecios = elementos.map((item) => {
+return {
+    id: item.id,
+    nombre: item.nombre,
+    precio: item.precio - item.precio * 0.21,
+    };
+});
+
+console.log(nuevosPrecios);
+
+//Despliegue de detalle de elementos y parámetros en arreglo
+elementos.forEach((item) => {
+console.log(item.nombre);
+console.log(item.precio);
+console.log(item.id);
+});
+
+// Buscador de objeto en arreglo
+const objetoElegido = elementos.find((item) => item.nombre ==="Guitarra");
+console.log(objetoElegido);
+
+//filtro de precios a consultar por productos en la página
+const filtrados = elementos.filter((item) => item.precio > 7000);
+console.log(filtrados);
+
+let precio = parseInt(prompt("Ingrese el precio minimo del producto a consultar"));
+
+const filtros = elementos.filter((item) => item.precio > precio);
+
+filtros.forEach((item) => {
+let mensaje = `
+    Id: ${item.id}
+    Nombre: ${item.nombre}
+    Precio: $${item.precio}
+`;
+
+alert(mensaje);
+});
+
+//acumulador de valores totales del carrito de compras
+const total = elementos.reduce((acumulador, item) => acumulador + item.precio, 0);
+console.log(total);
